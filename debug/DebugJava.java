79b3841e0d775d7c6f43b71ba263d82ba3043e3f
huy12345a <Quanghuy.hmtb@gmail.com>
2024-06-26 14:12:44 +0700

    NumberFormatException

--- debug/DebugJava.java
+++ debug/DebugJava.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DebugJava {
     public static void main(String[] args) {
-        System.out.println("Linear Equation Resolver");
+        System.out.      println("Linear Equation Resolver");
         System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
 
         Scanner scanner = new Scanner(System.in);
